d8d54505eea482c5d39420118befd9e25f13abb5
Aasifqureshi786 <zeroxgamer775@gmail.com>
2025-11-05 22:42:02 -0800

    Add files via upload
    initial website files

---
+++ script.js
@@ -0,0 +1,39 @@
+//Artimetic operators
+let a = 8;
+let b = 8;
+console.log("hello world");
+console.log("a=",a, "& b=",b,);
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+
+//unary operator
+alert("https://www.instagram.com/edit_mufasa7?igsh=MWhoa21veGc5OTR5eQ==");
+//radius
+name = "MUFASA GAMER";
+age = 18;
+isfollow = true;
+radius = 14;
+console.log(name);
+console.log(age);
+console.log(isfollow);
+console.log(radius);
+a = null;
+a = undefined;
+let x = BigInt("786");
+const student = {
+    fullname : "AASIF",
+    age: 18,
+    cgpa: 7.0,
+    ispass: true,
+};
+console.log(student["age"]);
+const product = {
+    title: "Ball Pen",
+    rating: 4.5,
+    offer: 3,
+    price: 150,
+};
+console.log (product);
